f7556b18cf29c0ef1f9e656f05f6ea07693e16c6
khaled boukhris <boukhriskhaled@outlook.com>
2020-09-02 12:59:26 +0100

    modified header.component.ts

--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   today: number = Date.now();
-  name: string = 'salim';
+  name: string = 'khaled';
 
   constructor() { }
 
